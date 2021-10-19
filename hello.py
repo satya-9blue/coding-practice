0baed34daa439af844f1ed96f2231e7deabef6b2
satya-9blue <satyamortha10@gmail.com>
2021-10-19 20:50:46 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello")
+num1=int(input())
+num2=int(input())
+if num1>num2:
+    print("num1 is greater than num2")
+elif num1<num2:
+    print("num1 is less than num2")
+else:
+    print("both are same")
